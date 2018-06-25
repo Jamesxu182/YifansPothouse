049ddea112d8296b3b5ef10a211870c642db462b
james <james@DESKTOP-3CVNRDH>
2018-06-25 23:13:05 +0100

    Remove useless imported packages from auth server

--- YifansAuthServer/src/main/java/com/james/auth/server/YifansAuthServerApplication.java
+++ YifansAuthServer/src/main/java/com/james/auth/server/YifansAuthServerApplication.java
@@ -3,7 +3,6 @@ package com.james.auth.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(
